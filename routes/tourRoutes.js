236e6ec07a421616db9241f8c021c7367b724515
Joao Humberto <joaohbmenezes@outlook.com>
2022-04-13 17:42:47 -0300

    refactor: controller files

---
+++ routes/tourRoutes.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const tourController = require('./../controllers/tourController')
+
+const router = express.Router()
+
+
+router.route('/')
+    .get(tourController.getAllTours)
+    .post(tourController.postTour)
+
+router.route('/:id')
+    .get(tourController.getTour)
+    .patch(tourController.patchTour)
+    .delete(tourController.deleteTour)
+
+module.exports = router
