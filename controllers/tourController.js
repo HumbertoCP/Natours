e451af4d8bfa55f3838495885733433ee37c5f46
Joao Humberto <joaohbmenezes@outlook.com>
2022-04-19 14:01:03 -0300

    feature: pagination

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -21,13 +21,27 @@ exports.getAllTours = async (req, res) => {
             query = query.sort('-createdAt')
         }
 
-        //field limiting
+        // Field Limiting
         if (req.query.fields){
             const fields = req.query.fields.split(',').join(' ')
             query = query.select(fields)
         } else{
             query = query.select('-__v')
         }
+
+        //Pagination
+        const page = req.query.page * 1 || 1 //convert string to number
+        const limit = req.query.limit *1 || 100
+        const skip = limit * (page - 1)
+
+        query = query.skip(skip).limit(limit)
+
+        if(req.query.page) {
+            const numTours = await Tour.countDocuments()
+            if(skip >= numTours) throw new Error('This page does not exist')
+        }
+
+        //EXECUTE QUERY
         const tours = await query
 
         res.status(200).json({
@@ -39,7 +53,10 @@ exports.getAllTours = async (req, res) => {
         })
     }
     catch(err){
-        
+       res.status(404).json({
+           status: 'fail',
+           message: err
+       }) 
     }
     
 }
