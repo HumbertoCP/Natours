080c2dfe6b621449fea7282364c264ad877c09c0
Joao Humberto <joaohbmenezes@outlook.com>
2022-04-19 16:18:12 -0300

    refactor: filter, sort

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,48 +1,14 @@
 const Tour = require('./../models/tourModel')
-
+const APIFeatures = require('./../utils/apiFeatures')
 exports.getAllTours = async (req, res) => {
     try {
-        const queryObj = {...req.query}
-        const excludedFields = ['page', 'sort', 'limit', 'fields']
-        excludedFields.forEach(el => delete queryObj[el])
-        
-        let queryStr = JSON.stringify(queryObj)
-        queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`)
-        console.log(JSON.parse(queryStr))
-
-        let query = Tour.find(JSON.parse(queryStr))
-        
-        // sorting
-        if(req.query.sort){
-            const sortBy = req.query.sort.split(',').join(' ')
-            console.log(sortBy)
-            query = query.sort(sortBy)
-        } else{
-            query = query.sort('-createdAt')
-        }
-
-        // Field Limiting
-        if (req.query.fields){
-            const fields = req.query.fields.split(',').join(' ')
-            query = query.select(fields)
-        } else{
-            query = query.select('-__v')
-        }
-
-        //Pagination
-        const page = req.query.page * 1 || 1 //convert string to number
-        const limit = req.query.limit *1 || 100
-        const skip = limit * (page - 1)
-
-        query = query.skip(skip).limit(limit)
-
-        if(req.query.page) {
-            const numTours = await Tour.countDocuments()
-            if(skip >= numTours) throw new Error('This page does not exist')
-        }
-
         //EXECUTE QUERY
-        const tours = await query
+        const features = new APIFeatures(Tour.find(), req.query)
+            .filter()
+            .sort()
+            .limitFields()
+            .paginate();
+        const tours = await features.query;
 
         res.status(200).json({
             status: 'success',
@@ -52,60 +18,57 @@ exports.getAllTours = async (req, res) => {
             }
         })
     }
-    catch(err){
-       res.status(404).json({
-           status: 'fail',
-           message: err
-       }) 
+    catch(err) {
+        console.log(err)
     }
-    
+
 }
 exports.getTour = async (req, res) => {
-    try{
+    try {
         const tour = await Tour.findById(req.params.id)
-        
+
         res.status(200).json({
             status: 'success',
             data: {
                 tour: tour
             }
-        }) 
+        })
     }
-    catch{
+    catch {
         res.status(404).json({
             status: 'fail',
             message: err
         })
     }
 
-    
+
 }
 
 exports.createTour = async (req, res) => {
-    try{
+    try {
         const newTour = await Tour.create(req.body)
 
-    res.status(201).json({
-        status: 'success',
-        data: {
-            tour: newTour
-        }
-    })
+        res.status(201).json({
+            status: 'success',
+            data: {
+                tour: newTour
+            }
+        })
     }
 
-    catch(err){
+    catch (err) {
         res.status(400).json({
             status: 'fail',
             message: err
         })
     }
 
-    
+
 }
 
 exports.updateTour = async (req, res) => {
 
-    try{
+    try {
         const tour = await Tour.findByIdAndUpdate(req.params.id, req.body, {
             new: true,
             runValidators: true
@@ -118,7 +81,7 @@ exports.updateTour = async (req, res) => {
         })
     }
 
-    catch(err){
+    catch (err) {
         res.status(400).json({
             status: 'fail',
             message: err
@@ -127,7 +90,7 @@ exports.updateTour = async (req, res) => {
 }
 
 exports.deleteTour = async (req, res) => {
-    try{
+    try {
         await Tour.findByIdAndDelete(req.params.id)
 
         res.status(204).json({
@@ -136,7 +99,7 @@ exports.deleteTour = async (req, res) => {
         })
     }
 
-    catch(err){
+    catch (err) {
         res.status(400).json({
             status: 'fail',
             message: err
