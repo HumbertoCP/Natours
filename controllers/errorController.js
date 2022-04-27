e10c7ef27ac7445abd4c31c3135717dd64faf60f
Joao Humberto <joaohbmenezes@outlook.com>
2022-04-27 11:26:15 -0300

    feature: user auth

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -8,7 +8,7 @@ const handleCastErrorDB = err => {
 const handleDuplicateFieldsDB = err => {
     const value = err.errmsg.match(/(["'])(\\?.)*?\1/)[0];
     console.log(value);
-  
+
     const message = `Duplicate field value: ${value}. Please use another value!`;
     return new AppError(message, 400);
 };
@@ -31,40 +31,40 @@ const sendErrorDev = (err, res) => {
 const sendErrorProd = (err, res) => {
     // Operational, trusted error: send message to client
     if (err.isOperational) {
-    res.status(err.statusCode).json({
-        status: err.status,
-        message: err.message
-    });
+        res.status(err.statusCode).json({
+            status: err.status,
+            message: err.message
+        });
 
-    // Programming or other unknown error: don't leak error details
+        // Programming or other unknown error: don't leak error details
     } else {
-    // 1) Log error
-    console.error('ERROR 💥', err);
+        // 1) Log error
+        console.error('ERROR 💥', err);
 
-    // 2) Send generic message
-    res.status(500).json({
-        status: 'error',
-        message: 'Something went very wrong!'
-    });
+        // 2) Send generic message
+        res.status(500).json({
+            status: 'error',
+            message: 'Something went very wrong!'
+        });
     }
 };
 
 module.exports = (err, req, res, next) => {
     // console.log(err.stack);
-  
+
     err.statusCode = err.statusCode || 500;
     err.status = err.status || 'error';
-  
+
     if (process.env.NODE_ENV === 'development') {
         sendErrorDev(err, res);
     } else if (process.env.NODE_ENV === 'production') {
         let error = Object.assign(err);
-  
+
         if (error.name === 'CastError') error = handleCastErrorDB(error);
         if (error.code === 11000) error = handleDuplicateFieldsDB(error);
         if (error.name === 'ValidationError')
-        error = handleValidationErrorDB(error);
-  
+            error = handleValidationErrorDB(error);
+
         sendErrorProd(error, res);
     }
 };
---
+++ models/userModel.js
@@ -0,0 +1,54 @@
+const mongoose = require('mongoose');
+const validator = require('validator')
+const bcrypt = require('bcryptjs')
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, 'Name is required'],
+        trim: true,
+        maxlenght: [40, 'A name must have less or equal then 40 characters'],
+        minlenght: [10, 'A name must have more or equal then 10 characters']
+    },
+    email: {
+        type: String,
+        required: [true, 'Email is required'],
+        unique: true,
+        trim: true,
+        lowercase: true,
+        validate: [validator.isEmail, 'Provide a valid email']
+    },
+    photo: String,
+
+    password: {
+        type: String,
+        required: [true, 'Password is required'],
+        minlenght: 8
+    },
+    confirmPassword: {
+        type: String,
+        required: [true, 'Password confirmation is required'],
+        validate: {
+            validator: function(el){
+                return el === this.password
+            },
+            message: 'Passwords are not the same'
+        }
+    }
+})
+
+userSchema.pre('save', async function(next) {
+    // Only run this function if password was modified
+    if(!this.isModified('password')) return next()
+
+    // Hash password with coast of 12
+    this.password = await bcrypt.hash(this.password, 12)
+
+    // Delete confirmPassword field
+    this.confirmPassword = undefined
+    next()
+})
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User
